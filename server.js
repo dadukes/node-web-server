8165c52b677c36da99abd6d817c8f2a01c3ac90c
Duke <dadukes3@gmail.com>
2018-04-17 18:20:27 +0200

    Projects page created

--- server.js
+++ server.js
@@ -19,10 +19,10 @@ app.use((req, res, next) =>{
     next();
 });
 
-app.use((req, res, next) =>{
-    res.render("maintenance.hbs");
-    //next();
-});
+// app.use((req, res, next) =>{
+//     res.render("maintenance.hbs");
+//     //next();
+// });
 
 app.use(express.static(__dirname + "/public"));
 
@@ -44,6 +44,12 @@ app.get("/", (req, res) => {
 app.get("/about", (req, res) => {
     res.render("about.hbs", {
         pageTitle: "About Page"
+    });
+});
+
+app.get("/projects", (req, res) => {
+    res.render("projects.hbs", {
+        pageTitle: "Projects Page"
     });
 });
 
